cfd62602a06eb032d41f01501921de4aa31624d4
samarkun <samarkun4@gmail.com>
2025-12-15 08:54:22 -0500

    basic monorepo and db prisma setup

---
+++ apps/hooks/src/index.ts
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+
+// TODO : Passoword logic make sure not random person hittin this endpoint
+app.post("/hooks/catch/:userId/:zapId", (req, res) => {
+  const userId = req.params.userId;
+  const zapId = req.params.zapId;
+
+  //store in db a new trigger 
+  //push it on to a queue (kafka/redis)
+
+
+})
