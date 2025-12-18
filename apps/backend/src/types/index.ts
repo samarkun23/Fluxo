e1c3e92e4f5825f3a3c213dc6ff020168316b65d
samarkun <samarkun4@gmail.com>
2025-12-18 07:56:43 -0500

    create zap type schema

--- apps/backend/src/types/index.ts
+++ apps/backend/src/types/index.ts
@@ -12,3 +12,12 @@ export const signInSchema = z.object({
     password: z.string().min(5, "Password is too short")
 })
 
+export const zapCreateSchema = z.object({
+    availableTriggerId: z.string(),
+    triggerMetadata: z.any().optional(),
+    actions: z.array(z.object({
+        availableactionId: z.string(),
+        actionMetadata: z.any().optional()
+    }))
+})
+
